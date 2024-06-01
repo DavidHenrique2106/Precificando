e9b5b33560ecf237939417b8c3cd7387636a33bb
David Pontes <60227838+Davidbjjj@users.noreply.github.com>
2024-06-01 05:11:49 -0300

    refactored: roles

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: LoginPage(),
       routes: {
         '/home': (context) => MyHomePage(),
         '/ingredientes': (context) => IngredientListPage(),
