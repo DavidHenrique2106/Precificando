52f355f09f6340ecc5f23faa731b53d410677fc9
David Pontes <60227838+Davidbjjj@users.noreply.github.com>
2024-06-02 08:22:09 -0300

    delete:comparison homePage

--- lib/homeUser/home_page.dart
+++ lib/homeUser/home_page.dart
@@ -19,7 +19,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _onItemTapped(int index) {
     setState(() {
       if (index == 1) {
-
+        // Lógica para index 1, se necessário
       } else if (index == 2) {
         Navigator.push(
           context,
@@ -58,8 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      backgroundColor: Color(0xffe3e3e3),
+      backgroundColor: Color(0x1fdbdbdb),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -406,7 +405,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
           ],
         ),
       ),
@@ -416,130 +414,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 caption: TextStyle(color: Colors.white),
               ),
         ),
-
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
-              color: Colors.transparent,
-              child: Center(
-                child: Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.white,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Container Centralizado',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                            height: 20), // Espaço entre o texto e os botões
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              icon: Icon(
-                                  Icons.restaurant_menu), // Ícone de cardápio
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          IngredientListPage()),
-                                );
-                                ; // Ação ao pressionar o botão de cardápio
-                              },
-                            ),
-                            SizedBox(width: 20), // Espaço entre os botões
-                            IconButton(
-                              icon:
-                                  Icon(Icons.shopping_bag), // Ícone de produto
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ProductListPage()),
-                                );
-
-                                // Ação ao pressionar o botão de produto
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(114, 133, 202, 1),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey,
-                    ),
-                    child: Icon(
-                      Icons.person,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Flora Matos',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Empresa XYZ',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Theme(
-        data: Theme.of(context).copyWith(
-          textTheme: Theme.of(context).textTheme.copyWith(
-                bodySmall: TextStyle(color: Colors.white),
-              ),
-        ),
-
         child: ClipRRect(
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(16.0),
@@ -551,10 +425,7 @@ class _MyHomePageState extends State<MyHomePage> {
               BottomNavigationBarItem(
                 icon: _buildIconWithLabel(Icons.home, _selectedIndex == 0),
                 label: 'Home',
-
                 backgroundColor: Colors.white,
-
-
               ),
               BottomNavigationBarItem(
                 icon: _buildIconWithLabel(
@@ -562,22 +433,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   _selectedIndex == 1,
                 ),
                 label: 'Finanças',
-
                 backgroundColor: Colors.white,
-
               ),
               BottomNavigationBarItem(
                 icon: _buildIconWithLabel(Icons.person, _selectedIndex == 2),
                 label: 'Perfil',
-
                 backgroundColor: Colors.white,
               ),
             ],
             selectedItemColor: Colors.white,
-            unselectedItemColor: Colors.white,
-
-              ),
-            ],
+            unselectedItemColor: Colors.white70,
             currentIndex: _selectedIndex,
             onTap: _onItemTapped,
           ),
