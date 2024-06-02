1735f9bca9508f902eaf0b5a233df2ee57fbf6d6
David Pontes <60227838+Davidbjjj@users.noreply.github.com>
2024-06-01 22:09:33 -0300

    fix:first route

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfilePage(),
+      home: LoginPage(),
       routes: {
         '/home': (context) => MyHomePage(),
         '/ingredientes': (context) => IngredientListPage(),
